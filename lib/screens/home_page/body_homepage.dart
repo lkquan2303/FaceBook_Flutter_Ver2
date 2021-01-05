741e2d3f7788070c3ace5acf8bfb84178c8e2bf8
Quan (Uri) K.LE <uri@enclave.vn>
2021-01-05 11:19:59 +0700

    Implement User Story

--- lib/screens/home_page/body_homepage.dart
+++ lib/screens/home_page/body_homepage.dart
@@ -1,11 +1,10 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:facebook_flutter_ver2/models/user_model.dart';
 import 'package:flutter/material.dart';
-import '../../config/size_config.dart';
 import '../../models/story_model.dart';
 import 'components/button_social_action.dart';
-import 'components/online_user.dart';
+import 'components/user_online.dart';
 import 'components/user_status.dart';
+import 'components/user_story.dart';
 
 class BodyHomePage extends StatelessWidget {
   const BodyHomePage({
@@ -31,62 +30,10 @@ class BodyHomePage extends StatelessWidget {
             const SizedBox(height: 5.0),
             OnlineUser(usersOnline: usersOnline),
             const SizedBox(height: 5.0),
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Row(
-                children: List.generate(
-                  stories.length + 1,
-                  (index) {
-                    // if (index == 0) {
-                    //   return Container(
-                    //     margin: const EdgeInsets.symmetric(horizontal: 8.0),
-                    //     height: 200,
-                    //     child: StoryCard(
-                    //       currentUser: currentUser, story: stories[index],
-                    //     ),
-                    //   );
-                    // }
-                    return Container(
-                      margin: const EdgeInsets.symmetric(horizontal: 8.0),
-                      height: getProportionateScreenWidth(160),
-                      child: StoryCard(
-                        currentUser: currentUser,
-                        //  story: stories[index],
-                      ),
-                    );
-                  },
-                ),
-              ),
-            ),
+            UserStory(stories: stories, currentUser: currentUser),
           ],
         ),
       ),
-    );
-  }
-}
-
-class StoryCard extends StatelessWidget {
-  final User currentUser;
-  final Story story;
-  final bool isAddStory;
-
-  const StoryCard({Key key, this.currentUser, this.story, this.isAddStory})
-      : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        ClipRRect(
-          borderRadius: BorderRadius.circular(20),
-          child: CachedNetworkImage(
-            imageUrl: currentUser.imageUrl,
-            fit: BoxFit.cover,
-            height: double.infinity,
-            width: getProportionateScreenWidth(100),
-          ),
-        ),
-      ],
     );
   }
 }
---
+++ lib/screens/home_page/components/user_story.dart
@@ -0,0 +1,134 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:facebook_flutter_ver2/config/constants.dart';
+import 'package:facebook_flutter_ver2/config/size_config.dart';
+import 'package:facebook_flutter_ver2/models/story_model.dart';
+import 'package:facebook_flutter_ver2/models/user_model.dart';
+import 'package:flutter/material.dart';
+
+class UserStory extends StatelessWidget {
+  const UserStory({
+    Key key,
+    @required this.stories,
+    @required this.currentUser,
+  }) : super(key: key);
+
+  final List<Story> stories;
+  final User currentUser;
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: List.generate(
+          stories.length + 1,
+          (index) {
+            if (index == 0) {
+              return Container(
+                margin: const EdgeInsets.symmetric(horizontal: 8.0),
+                height: getProportionateScreenWidth(180),
+                child: StoryCard(
+                  currentUser: currentUser,
+                  isAddStory: true,
+                ),
+              );
+            }
+            return Container(
+              margin: const EdgeInsets.symmetric(horizontal: 8.0),
+              height: getProportionateScreenWidth(180),
+              child: StoryCard(
+                story: stories[index - 1],
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class StoryCard extends StatelessWidget {
+  final User currentUser;
+  final Story story;
+  final bool isAddStory;
+
+  const StoryCard({
+    Key key,
+    this.story,
+    this.currentUser,
+    this.isAddStory = false,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(20),
+          child: CachedNetworkImage(
+            imageUrl: isAddStory ? currentUser.imageUrl : story.imageUrl,
+            fit: BoxFit.cover,
+            height: double.infinity,
+            width: getProportionateScreenWidth(100),
+          ),
+        ),
+        IconStory(
+          isAddStory: isAddStory,
+          story: story,
+        ),
+        Positioned(
+          bottom: 2,
+          left: 8,
+          right: 8,
+          child: Text(
+            isAddStory ? "Add to story" : story.user.name,
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class IconStory extends StatelessWidget {
+  final bool isAddStory;
+  final Story story;
+  const IconStory({Key key, this.isAddStory, this.story}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      top: 4,
+      left: 4,
+      child: isAddStory
+          ? Container(
+              height: getProportionateScreenHeight(45),
+              width: getProportionateScreenHeight(45),
+              decoration:
+                  BoxDecoration(color: Colors.white, shape: BoxShape.circle),
+              child: IconButton(
+                icon: Icon(Icons.add),
+                color: kPrimaryColor,
+                iconSize: getProportionateScreenHeight(30),
+                onPressed: () {},
+              ),
+            )
+          : Container(
+              decoration: BoxDecoration(
+                border: Border.all(
+                    width: 2.5,
+                    color: story.isViewed ? Colors.transparent : Colors.red),
+                shape: BoxShape.circle,
+              ),
+              child: CircleAvatar(
+                backgroundImage: CachedNetworkImageProvider(
+                  story.user.imageUrl,
+                ),
+                backgroundColor: Colors.white,
+              ),
+            ),
+    );
+  }
+}
